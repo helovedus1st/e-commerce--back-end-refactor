b689b2b6b1ef0691643fcfeb1c74b74bfb41bdd9
JC Renner <jc-renner@lu-tec.com>
2021-10-29 20:59:55 -0400

    category all and category id routes working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,34 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: {
+        model: Product
+      }
+    })
+    res.status(200).json(categories)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoriesId = await Category.findByPk(req.params.id, {
+      include: {
+        model: Product
+      }
+    })
+    res.status(200).json(categoriesId)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
