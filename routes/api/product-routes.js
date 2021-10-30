084bae653064f34a0f161e831a6bb2de48436874
JC Renner <jc-renner@lu-tec.com>
2021-10-29 23:56:37 -0400

    product delete added and working, added sequelize sync to server

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -122,8 +122,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!deleteProduct) {
+      res.status(404).json({message: "No products with that ID"})
+    }
+    res.status(200).json({message: `Product id: ${req.params.id} has been deleted.`})
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
